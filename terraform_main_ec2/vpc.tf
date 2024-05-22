6087e16cde2f63d43869d49e98db5676da672a55
BALUOMKAR <159416286+BALUOMKAR@users.noreply.github.com>
2024-05-22 13:52:15 +0530

    Update vpc.tf

--- terraform_main_ec2/vpc.tf
+++ terraform_main_ec2/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_subnet" "public-subnet" {
+resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
@@ -25,7 +25,7 @@ resource "aws_subnet" "public-subnet" {
   }
 }
 
-resource "aws_subnet" "public-subnet" {
+resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
